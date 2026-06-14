999717993edd0993e3973312be372dda585038a4
unknown <lubnabatul02@gmail.com>
2026-06-14 10:58:14 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
+from tools import search_tool
+load_dotenv()
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+    
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
+parser=PydanticOutputParser(pydantic_object=ResearchResponse)
+prompt=ChatPromptTemplate.from_messages(
+    [
+   (
+"system",
+"""
+You are an expert research assistant.
+
+Use tools when necessary.
+
+If a tool does not return useful information, answer from your own knowledge.
+
+Always return valid JSON.
+
+{format_instructions}
+"""
+),
+        ("placeholder","{chat_history}"),
+        ("human","{query}"),
+        ("placeholder","{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+tools=[search_tool]
+agent=create_tool_calling_agent(
+llm=llm,
+prompt=prompt,
+tools=tools
+)
+
+agent_executor=AgentExecutor(
+    agent=agent,
+    tools=tools,
+    verbose=True
+)
+query=input("What can i help you research? ")
+raw_response=agent_executor.invoke({"query": query})
+
+
+
+try:
+    strucuted_response=parser.parse(raw_response["output"])
+    print(strucuted_response)
+except Exception as e:
+    print("Error parsing response", e, "Raw Response - ", raw_response)
---
+++ tools.py
@@ -0,0 +1,23 @@
+from langchain.tools import Tool
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+#duckdduckgo search
+search=DuckDuckGoSearchRun()
+
+search_tool=Tool(
+    name="web_search",
+    func=search.run,
+    description="search the web for info"
+)
+
+#wikipedia search 
+
+wiki = WikipediaQueryRun(
+    api_wrapper = WikipediaAPIWrapper()
+)
+wiki_tool = Tool(
+    name="wiki_search",
+    func=wiki.run,
+    description="Search Wikipedia for information."
+)
